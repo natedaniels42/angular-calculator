28000b3a6757fae4c8e02f8ca249dde3ac814355
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-30 15:20:36 -0600

    Moved fixture, app, and displayText definitions to beforeEach

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let displayText: HTMLInputElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,123 +17,92 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    displayText = (document.getElementById('displayText') as HTMLInputElement);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   describe('buttons', () => {
     it ('1 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num1');
       expect(button).toBeTruthy();
     });
 
     it ('2 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num2');
       expect(button).toBeTruthy();
     });
 
     it ('3 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num3');
       expect(button).toBeTruthy();
     });
 
     it ('4 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num4');
       expect(button).toBeTruthy();
     });
 
     it ('5 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num5');
       expect(button).toBeTruthy();
     });
 
     it ('6 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num6');
       expect(button).toBeTruthy();
     });
 
     it ('7 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num7');
       expect(button).toBeTruthy();
     });
 
     it ('8 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num8');
       expect(button).toBeTruthy();
     });
 
     it ('9 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num9');
       expect(button).toBeTruthy();
     });
 
     it ('0 button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('num0');
       expect(button).toBeTruthy();
     });
 
     it ('plus button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('plus');
       expect(button).toBeTruthy();
     });
 
     it ('minus button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('minus');
       expect(button).toBeTruthy();
     });
 
     it ('multiply button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('multiply');
       expect(button).toBeTruthy();
     });
 
     it ('divide button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('divide');
       expect(button).toBeTruthy();
     });
 
     it ('equal button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('equal');
       expect(button).toBeTruthy();
     });
 
     it ('clear button exists', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
       const button = document.getElementById('clear');
       expect(button).toBeTruthy();
     })
@@ -136,9 +110,6 @@ describe('AppComponent', () => {
 
   describe('addText', () => {
     it ('should add text on click', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       const displayValue = displayText.value;
       const clickedValue = (document.getElementById('num1') as HTMLElement).innerText;
       app.addText(clickedValue, displayText);
@@ -146,9 +117,6 @@ describe('AppComponent', () => {
     })
 
     it ('should not allow text to add on operator if operator is already in text', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '123+23';
       const displayValue = displayText.value;
       const clickedValue = (document.getElementById('plus') as HTMLElement).innerText;
@@ -157,9 +125,6 @@ describe('AppComponent', () => {
     })
 
     it ('should not allow text to be added if the length of text is already 18 characters', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '1234567890+1234567';
       const displayValue = displayText.value;
       const clickedValue = (document.getElementById('num1') as HTMLElement).innerText;
@@ -168,9 +133,6 @@ describe('AppComponent', () => {
     })
 
     it ('should clear the input first if calculated is true', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '1234';
       app.calculated = true;
       const clickedValue = (document.getElementById('num1') as HTMLElement).innerText;
@@ -181,9 +143,6 @@ describe('AppComponent', () => {
 
   describe('clear', () => {
     it ('should clear the input text', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '1234';
       app.clear(displayText);
       expect(displayText.value).toEqual('');
@@ -192,72 +151,48 @@ describe('AppComponent', () => {
 
   describe('evaluate', () => {
     it ('contains +', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '12+34';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('46');
     })
 
     it ('contains -', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '12-3';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('9');
     })
 
     it ('contains *', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '12*3';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('36');
     })
 
     it ('contains / and the number after is xero', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '12/0';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('Error');
     })
 
     it ('contains /', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '12/3';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('4');
     })
 
     it ('contains any character that is not a number or an operator (+-*/)', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = 'abc';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('Error');
     })
 
     it ('contains a number greater than the max safe integer', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '9999999999999999*9';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('Error');
     })
 
     it ('numbers total up to an amount above max safe integer', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      const displayText = (document.getElementById('displayText') as HTMLInputElement);
       displayText.value = '999999999999999*99';
       app.evaluate(displayText);
       expect(displayText.value).toEqual('Error');
